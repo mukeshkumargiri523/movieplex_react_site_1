ecea756c64017730eea4d5d70bb7d4edee8df35f
mukeshkumargiri523 <mukeshkumargiri523@gmail.com>
2023-12-11 09:31:58 +0530

    api modified

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 
-//const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
-const TMDB_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTQ4MmRiYzNkNzgyZTJlMDRiZWIxNjlmNThlN2YzOCIsInN1YiI6IjY0MzI0ZjhmMzEwMzI1MDBiZDU2OWJlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L6j02mCfh5qY1yfxsRNSkEHRxBlwCSI-MyBjIQL_GiA";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
